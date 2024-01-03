5174c99bde97e235c08b0fec1a04b99f8f213a96
jxdang1 <dangjennifer21@gmail.com>
2024-01-02 18:46:26 -0600

    created handlebars, routes

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const users = require('./user-routes');
+const upgrade = require('./upgrade-routes');
+const monsters = require('./monster-routes');
+const games = require('./game-routes');
+
+router.use('./games', games);
+router.use('/upgrade', upgrade);
+router.use('/monsters', monsters);
+router.use('/users', users);
+
+module.exports = router;
