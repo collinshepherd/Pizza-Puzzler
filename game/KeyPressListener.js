c06cbb6846d7f9cae86b3cc181c59d3e7b53bcbc
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2024-01-08 19:32:24 -0600

    Adding all files for game

---
+++ game/KeyPressListener.js
@@ -0,0 +1,26 @@
+class KeyPressListener {
+  constructor(keyCode, callback) {
+    let keySafe = true;
+    this.keydownFunction = function (event) {
+      if (event.code === keyCode) {
+        if (keySafe) {
+          keySafe = false;
+          callback();
+        }
+      }
+    };
+    this.keyUpFunction = function (event) {
+      if (event.code === keyCode) {
+        keySafe = true;
+      }
+    };
+
+    document.addEventListener("keydown", this.keydownFunction);
+    document.addEventListener("keyup", this.keyUpFunction);
+  }
+
+  unbind() {
+    document.removeEventListener("keydown", this.keydownFunction);
+    document.removeEventListener("keyup", this.keyUpFunction);
+  }
+}
